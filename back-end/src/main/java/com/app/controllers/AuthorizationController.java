de125af23bb1e378be9f8ae2f7a7a9a8e8e980e3
e.eremina <e.eremina@avystele.com>
2019-11-14 01:02:48 +0200

    #2: create Auth controller

--- back-end/src/main/java/com/app/controllers/AuthorizationController.java
+++ back-end/src/main/java/com/app/controllers/AuthorizationController.java
@@ -1,5 +1,22 @@
 package com.app.controllers;
 
+import com.app.configs.JwtTokenProvider;
+import com.app.services.AuthorizationService;
+import com.app.validators.UserValidator;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.web.bind.annotation.*;
+import sun.net.www.protocol.http.AuthenticationHeader;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/api/auth")
 public class AuthorizationController {
+
+   private AuthorizationService userService;
+   private UserValidator userValidator;
+   private JwtTokenProvider tokenProvider;
+   private AuthenticationManager authenticationManager;
+
+
 
 }
